7a1c4df5b516dfd4c729106865d83b8b4227f969
saci zakaria <sacideveloppeur@gmail.com>
2024-09-10 10:08:33 +0200

    builder: (context) =>

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -5,10 +5,10 @@ import 'package:flutter_fruits_hub/features/splash/presentation/views/splash_vie
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case 'splash':
-      return MaterialPageRoute(builder: (_) => const SplashView());
+      return MaterialPageRoute(builder: (context) => const SplashView());
     case 'onBoardingView':
-      return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
-      return MaterialPageRoute(builder: (_) => const Scaffold());
+      return MaterialPageRoute(builder: (context) => const Scaffold());
   }
 }
