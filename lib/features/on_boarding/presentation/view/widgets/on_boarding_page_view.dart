a2d0aa4700d81409df3cca7d3f5fbd37fb572faf
saci zakaria <sacideveloppeur@gmail.com>
2024-09-12 11:06:32 +0200

    return PageView

--- lib/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart
@@ -5,6 +5,18 @@ class OnBoardingPageview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PageView(
+      children: const [
+        Center(
+          child: Text('Page 1'),
+        ),
+        Center(
+          child: Text('Page 2'),
+        ),
+        Center(
+          child: Text('Page 3'),
+        ),
+      ],
+    );
   }
 }
