e0b1868b233003d6f8b7e7e5900357b667c80f69
saci zakaria <sacideveloppeur@gmail.com>
2024-09-16 17:13:23 +0200

    class PageviewItem extends StatelessWidget

---
+++ lib/features/on_boarding/presentation/view/widgets/page_view_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageviewItem extends StatelessWidget {
+  const PageviewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
