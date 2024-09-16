ed4626ff38ef19b672d2975f04668e1d9bdc197d
saci zakaria <sacideveloppeur@gmail.com>
2024-09-16 17:15:08 +0200

    final String image, background;

--- lib/features/on_boarding/presentation/view/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/view/widgets/page_view_item.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class PageviewItem extends StatelessWidget {
-  const PageviewItem({super.key});
+  const PageviewItem({
+    super.key,
+    required this.image,
+    required this.background,
+  });
+
+  final String image, background;
 
   @override
   Widget build(BuildContext context) {
