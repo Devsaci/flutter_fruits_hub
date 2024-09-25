4d79e48a62dac6536ae6ed8a974123bb81fdb1a3
saci zakaria <sacideveloppeur@gmail.com>
2024-09-25 15:00:23 +0200

    DotsIndicator

--- lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_fruits_hub/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Expanded(
+        const Expanded(
           child: OnBoardingPageview(),
+        ),
+        DotsIndicator(
+          dotsCount: 3,
         ),
       ],
     );
