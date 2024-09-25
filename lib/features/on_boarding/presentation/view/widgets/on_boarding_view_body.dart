42493f599d978499473b5fd862bce1d4b41eb87b
saci zakaria <sacideveloppeur@gmail.com>
2024-09-25 15:55:25 +0200

    activeColor: AppColors.primaryColor,

--- lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fruits_hub/core/utils/app_colors.dart';
 
 import 'package:flutter_fruits_hub/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart';
 import 'package:dots_indicator/dots_indicator.dart';
@@ -16,13 +17,7 @@ class OnBoardingViewBody extends StatelessWidget {
         DotsIndicator(
           dotsCount: 2,
           decorator: const DotsDecorator(
-            color: Color(0xFFC40303),
-            activeColor: Color(0xFF06A450),
-            size: Size(5, 8),
-            activeSize: Size(10, 8),
-            activeShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
+            activeColor: AppColors.primaryColor,
           ),
         ),
       ],
