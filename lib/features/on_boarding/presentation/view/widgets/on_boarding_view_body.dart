13062d428c1ce38517015e18740e1e497cc785d5
saci zakaria <sacideveloppeur@gmail.com>
2024-09-25 15:40:21 +0200

    DotsDecorator

--- lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter_fruits_hub/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 
@@ -15,6 +15,15 @@ class OnBoardingViewBody extends StatelessWidget {
         ),
         DotsIndicator(
           dotsCount: 3,
+          decorator: const DotsDecorator(
+            color: Color(0xFFC40303),
+            activeColor: Color(0xFF06A450),
+            size: Size(8, 8),
+            activeSize: Size(8, 8),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
         ),
       ],
     );
